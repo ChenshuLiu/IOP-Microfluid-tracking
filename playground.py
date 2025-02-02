f4b2b9268e80b0af89dc02c773af55cf855586ed
PinyiYang <chenshu0615@g.ucla.edu>
2025-02-02 14:26:06 -0500

    added second manual labeling step of defining the liquid surface by selecting the edge using few points

--- playground.py
+++ playground.py
@@ -10,10 +10,42 @@ import numpy as np
 
 cap = cv2.VideoCapture('Demo_vid.mp4')
 ret, frame = cap.read()
+# clicking event for selecting the points to define the liquid surface
+select_liquid_surface = False # won't start the selection unless after the bbox definition
+liquid_surface_define_points = []
+def define_liquid_surface(event, x, y, flags, param):
+    global liquid_surface_define_points, select_liquid_surface
+    if event == cv2.EVENT_LBUTTONDOWN and select_liquid_surface:
+        liquid_surface_define_points.append((x, y))
+
 bbox = cv2.selectROI("Choose microfluidic chamber", frame, fromCenter = False, showCrosshair = True)
 cv2.destroyWindow("Choose microfluidic chamber")
 tracker = cv2.TrackerKCF_create()
 tracker.init(frame, bbox)
+
+# after initializing the bbox tracker, allow user to define the liquid surface
+select_liquid_surface = True
+cv2.namedWindow("Define Liquid Surface")
+cv2.setMouseCallback("Define Liquid Surface", define_liquid_surface)
+while True:
+    frame_display = frame.copy()
+    # Draw the selected bounding box
+    x, y, w, h = map(int, bbox)
+    cv2.rectangle(frame_display, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # Draw selected points
+    for point in liquid_surface_define_points:
+        cv2.circle(frame_display, point, 1, (255, 0, 0), -1)
+
+    cv2.imshow("Define Liquid Surface", frame_display)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == 13:  # Press Enter to finish selecting points
+        break
+cv2.destroyWindow("Define Liquid Surface")  # Close point selection window
+selecting_points = False
+
+# tracking loop
 while True:
     ret, frame = cap.read() # already RGB video format
     if not ret:
